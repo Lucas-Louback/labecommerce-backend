aee3be137c055d20ae1fdb0ed9cf702fd4987078
Lucas-Louback <lucasmlouback99@gmail.com>
2023-07-05 15:48:40 -0300

    exercicios typescript-ii prontos

--- build/index.js
+++ build/index.js
@@ -2,4 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
 console.log("foi transpilado e executado com sucesso");
-console.log(database_1.users, database_1.products);
+(0, database_1.createNewUser)("u003", "Astrodev", "astrodev@email.com", "astrodev99");
+(0, database_1.createNewProduct)("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.", "google.com");
+(0, database_1.getAllUsers)();
+(0, database_1.getAllProducts)();
+(0, database_1.searchProductsByName)("gamer");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { users, products } from "./database";
+import { users, products, createNewUser, getAllUsers, createNewProduct, getAllProducts, searchProductsByName } from "./database";
 console.log("foi transpilado e executado com sucesso")
-
-console.log(users, products)
+createNewUser("u003", "Astrodev", "astrodev@email.com", "astrodev99")
+createNewProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.","google.com")
+getAllUsers()
+getAllProducts()
+searchProductsByName("gamer")
