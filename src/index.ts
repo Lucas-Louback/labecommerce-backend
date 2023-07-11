7a9ce2ae8ebc798af0c7ccd35083f71308bc24d4
Lucas-Louback <lucasmlouback99@gmail.com>
2023-07-11 14:00:29 -0300

    Exercicios apis-e-express prontos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { users, products, createNewUser, getAllUsers, createNewProduct, getAllProducts, searchProductsByName } from "./database";
-import express, { Request, Response} from 'express';
+import express, { Request, Response } from 'express';
+import { TUser, TProducts } from "./types";
 import cors from 'cors';
 
 const app = express();
@@ -12,13 +13,67 @@ app.listen(3003, () => {
 });
 
 app.get("/ping", (req: Request, res: Response) => {
-  res.send("Pong!");
+    res.send("Pong!");
 });
+
+app.get("/users", (req: Request, res: Response) => {
+    res.status(201).send(users)
+});
+
+app.get("/products", (req: Request, res: Response) => {
+    
+    const nameToFind = req.query.name as string
+
+    if (nameToFind) {
+        const result: TProducts[] = products.filter(
+            (product) => product.name.toLowerCase().includes(nameToFind.toLowerCase())
+        )
+        res.status(200).send(result)
+    }
+
+    res.status(200).send(products)
+});
+
+app.post('/users', (req: Request, res: Response) => {
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const email = req.body.email as string
+    const password = req.body.password as string
+
+    const newUser: TUser = {
+        id,
+        name,
+        email,
+        password,
+        createdAt: new Date().toISOString(),
+    }
+    users.push(newUser)
+    res.status(201).send("Cadastro realizado com sucesso")
+
+})
+
+app.post('/products', (req: Request, res: Response) => {
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const price = req.body.price as number
+    const description = req.body.description as string
+    const imageUrl = req.body.imageUrl as string
+
+    const newProduct: TProducts = {
+        id,
+        name,
+        price,
+        description,
+        imageUrl,
+    }
+    products.push(newProduct)
+    res.status(201).send("Cadastro realizado com sucesso")
+})
 
 
 console.log("foi transpilado e executado com sucesso")
 createNewUser("u003", "Astrodev", "astrodev@email.com", "astrodev99")
-createNewProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.","google.com")
+createNewProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.", "google.com")
 getAllUsers()
 getAllProducts()
 searchProductsByName("gamer")
