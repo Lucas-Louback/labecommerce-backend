d6d7a52b3d4d6886939304b9d7a5a5653aaabfaf
Lucas-Louback <lucasmlouback99@gmail.com>
2023-07-11 11:15:51 -0300

    Express configurado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import { users, products, createNewUser, getAllUsers, createNewProduct, getAllProducts, searchProductsByName } from "./database";
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("Pong!");
+});
+
+
 console.log("foi transpilado e executado com sucesso")
 createNewUser("u003", "Astrodev", "astrodev@email.com", "astrodev99")
 createNewProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.","google.com")
